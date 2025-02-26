92781664a5bac4103548b071caf029653c176197
Roy Allen Nidoy <allen.1120@icloud.com>
2025-02-26 23:32:47 +0800

    feat: implement filtering in get method

--- src/data/tasks.ts
+++ src/data/tasks.ts
@@ -1,7 +1,7 @@
 import { Task } from "../models/Task";
 
 // ** Map can store tasks with fast lookup **
-export const taskMap = new Map<string, Task>();
+export let taskMap = new Map<string, Task>();
 
 // ** Array can store task IDs in order **
-export const taskOrder: string[] = [];
+export let taskOrder: string[] = [];
